3460e22c6a5eb7cad7f0d7b95edce8d67e0d983f
Emre Akurek <emreakurek@MacBook-Air-2.local>
2018-09-21 17:48:42 -0500

    calculator-EvenOdd-Prime

---
+++ Even-Odd.py
@@ -0,0 +1,26 @@
+#def Even_Odd(number):
+#    if(number % 2 == 0):
+#      return "even"
+#    else:
+#       return "odd"
+
+
+
+#message = 'The number is ' + Even_Odd(8)
+
+#print(message)
+
+number = int(input('Enter your number: '))
+def Even_Odd():
+     if(number % 2 == 0):
+         print('Even')
+     elif(number % 2 == 1):
+         print('Odd')
+     else:
+         (None)
+Even_Odd()
+
+
+
+
+#        print('This number is odd')
